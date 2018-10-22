6c7c31ebe41e2a0e1b94667c15f17eb88eef214b
azrael8576 <azrael8576love2008@gmail.com>
2018-10-22 19:13:04 +0800

    Initial commit

---
+++ app/src/main/java/com/alex/atm/MainActivity.java
@@ -0,0 +1,167 @@
+package com.alex.atm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LOGIN = 100;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    boolean logon = false;
+    private List<Function> functions;
+
+    //    String [] functions = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (!logon){
+            Intent intent = new Intent(this,LoginActivity.class);
+            startActivityForResult(intent,REQUEST_LOGIN);
+//            startActivity(intent);
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        //Recycler
+        setupFunctions();
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this,3));
+        //Adapter
+//        FunctionAdapter adapter = new FunctionAdapter(this);
+        IconAdapter adapter = new IconAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setupFunctions() {
+        functions = new ArrayList<>();
+        String[] funes = getResources().getStringArray(R.array.functions);
+        functions.add(new Function(funes[0],R.drawable.func_transaction));
+        functions.add(new Function(funes[1],R.drawable.func_balance));
+        functions.add(new Function(funes[2],R.drawable.func_finance));
+        functions.add(new Function(funes[3],R.drawable.func_contacts));
+        functions.add(new Function(funes[4],R.drawable.func_exit));
+    }
+
+    public class IconAdapter extends RecyclerView.Adapter<IconAdapter.IconHolder>{
+
+        @NonNull
+        @Override
+        public IconHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view = getLayoutInflater().inflate(R.layout.item_icon,viewGroup,false);
+            return new IconHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull IconHolder iconHolder, int i) {
+            final Function function = functions.get(i);
+            iconHolder.nameText.setText(function.getName());
+            iconHolder.iconImage.setImageResource(function.getIcon());
+            iconHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemClicked(function);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return functions.size();
+        }
+
+        public class IconHolder extends RecyclerView.ViewHolder{
+            ImageView iconImage;
+            TextView nameText;
+            public IconHolder(@NonNull View itemView) {
+                super(itemView);
+                iconImage = itemView.findViewById(R.id.item_icon);
+                nameText = itemView.findViewById(R.id.item_name);
+            }
+        }
+    }
+
+    private void itemClicked(Function function) {
+        Log.d(TAG, "itemClicked: " + function.getName());
+        switch (function.getIcon()){
+            case R.drawable.func_transaction:
+                startActivity(new Intent(this,TransActivity.class));
+            break;
+            case R.drawable.func_balance:
+                break;
+            case R.drawable.func_finance:
+                Intent finance = new Intent(this,FinanceActivity.class);
+                startActivity(finance);
+                break;
+            case R.drawable.func_contacts:
+                Intent contacts = new Intent(this,ContactActivity.class);
+                startActivity(contacts);
+                break;
+            case R.drawable.func_exit:
+                finish();
+                break;
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==REQUEST_LOGIN){
+            if (resultCode != RESULT_OK){
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/alex/atm/NewsFragment.java
@@ -0,0 +1,90 @@
+package com.alex.atm;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewsFragment extends Fragment {
+    private static final String TAG = NewsFragment.class.getSimpleName();
+    private static NewsFragment instance;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: ");
+        return inflater.inflate(R.layout.fragment_news, container , false);
+    }
+
+    public static NewsFragment getInstance() {
+        if (instance == null){
+            instance = new NewsFragment();
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onActivityCreated: ");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.d(TAG, "onStart: ");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.d(TAG, "onResume: ");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.d(TAG, "onPause: ");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.d(TAG, "onDestroyView: ");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d(TAG, "onDetach: ");
+        super.onDetach();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        Log.d(TAG, "onAttach: ");
+        super.onAttach(context);
+    }
+}
