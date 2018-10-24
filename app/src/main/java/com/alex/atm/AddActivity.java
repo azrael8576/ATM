806420bbeb891b44d9254292c9536ba26a0fec17
azrael8576 <azrael8576love2008@gmail.com>
2018-10-24 19:55:02 +0800

    ˙ 修復 主頁面>投資理財>新增 異常

--- app/src/main/java/com/alex/atm/AddActivity.java
+++ app/src/main/java/com/alex/atm/AddActivity.java
@@ -29,20 +29,25 @@ public class AddActivity extends AppCompatActivity {
     public void add(View view){
         String date = edDate.getText().toString();
         String info = edInfo.getText().toString();
-        int amount = Integer.parseInt(edAmount.getText().toString());
-        ExpenseHelper helper = new ExpenseHelper(this);
-        ContentValues values = new ContentValues();
-        values.put("cdate",date);
-        values.put("info",info);
-        values.put("amount",amount);
-        long id = helper.getWritableDatabase()
-                .insert("expense",null,values);
-        if (id > -1){
-            Toast.makeText(this,"新增成功",Toast.LENGTH_LONG).show();
+        int amount = 0;
+        if (!"".equals(edAmount.getText().toString())){
+            amount = Integer.parseInt(edAmount.getText().toString());
+            ExpenseHelper helper = new ExpenseHelper(this);
+            ContentValues values = new ContentValues();
+            values.put("cdate",date);
+            values.put("info",info);
+            values.put("amount",amount);
+            long id = helper.getWritableDatabase()
+                    .insert("expense",null,values);
+            if (id > -1){
+                Toast.makeText(this,"新增成功",Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(this,"新增失敗",Toast.LENGTH_LONG).show();
+            }
         }
         else{
             Toast.makeText(this,"新增失敗",Toast.LENGTH_LONG).show();
         }
-
     }
 }
